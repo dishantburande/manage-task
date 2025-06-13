023c2d8d858e771c3009654d29b1c80c43266eda
Dishant Burande <dishant.burande@gmail.com>
2025-06-13 15:59:59 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import Connection from "./database/db.js";
-
+import fileUpload from "express-fileupload";
+import cloudinary from 'cloudinary';
+import { Router } from "./routes/route.js";
 dotenv.config();
 
 const app = express();
@@ -20,9 +22,24 @@ app.use(express.urlencoded({ extended: true }));
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use('/api/user/v1', Router)
+
 const PORT = 8000;
 
 Connection(USERNAME, PASSWORD);
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+  api_key: process.env.CLOUDINATRY_CLIENT_API,
+  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+});
 
 app.listen(PORT, ()=> console.log(`server is runing on port ${PORT}`));
 
